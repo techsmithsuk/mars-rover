a4ec671c0d0e286eb32f41ecb3b271baf6638e58
Jake Horton <jake.horton@softwire.com>
2019-11-26 12:19:29 +0000

    refactored sass

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,15 +2,11 @@ import React,{useState} from 'react';
 import './style/home.scss';
 
 export function HomePage() {
-const[homeHeight, setHomeHeight] = useState( {height:"100vh"})
 
-function setHomeHeighttoZero(){
-    setHomeHeight({height:"100px"});
-}
 
     return (
-        <div >
-            <section className="home" onClick={setHomeHeighttoZero} style={homeHeight}> Welcome to the Mars Rover!</section>
+        <div>
+            <section className="home" > Welcome to the Mars Rover!</section>
             <div className="sections">
                 <section className="missions">Missions</section>
                 <section className="weather">Weather</section>
