ff79f3407be7373ad13c0d21249e29341081260c
Jake Horton <jake.horton@softwire.com>
2019-11-26 13:49:03 +0000

    sass naming changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,11 @@
-import React,{useState} from 'react';
-import './style/home.scss';
+import React from 'react';
+import './home.scss';
 
 export function HomePage() {
 
 
     return (
-        <div>
+        <div className="homePage">
             <section className="home" > Welcome to the Mars Rover!</section>
             <div className="sections">
                 <section className="missions">Missions</section>
