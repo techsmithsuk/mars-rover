b8066a60b8b0aea94a950239795be40e6c43eb62
ChrisBRN <>
2019-11-26 16:24:26 +0000

    Basic Card Styling...

--- src/components/weather-page/SingleDaySummary.tsx
+++ src/components/weather-page/SingleDaySummary.tsx
@@ -11,6 +11,13 @@ import WindIcon from './resources/svg_icon_wind';
 // TODO - Add function to convert "2019-11-14T16:08:54Z" into weekday i.e. Monday
 
 export function SingleDaySummary(props : SingleDayData) {
+ 
+    let min = props.minTemp ? Math.round(props.minTemp) : 0;
+    let max = props.maxTemp ? Math.round(props.maxTemp) : 0;
+    let avTemp = props.averageTemp ? Math.round(props.averageTemp) : 0;
+
+
+    
 
     return (
         <div className="SingleDaySummary">
@@ -22,18 +29,17 @@ export function SingleDaySummary(props : SingleDayData) {
                 </div>
 
                 <div className="IconContainer">
-                    <WindIcon/> 
+                    <CloudIcon/> 
                 </div>
                 
                 <div className="TempContainer">
-                    <div>{props.minTemp}</div>
-                    <div>{props.maxTemp}</div>
+                    <div>{avTemp}</div>                    
                 </div>                
 
             </div>
 
-            <div className="TextSummary">
-                <p>
+            <div className="TextSummaryContainer">
+                <p className="SummaryText">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                 </p>
