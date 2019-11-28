fd72906f49fafb66cace5107dc98c408b5adffcc
Jake Horton <jake.horton@softwire.com>
2019-11-28 16:38:35 +0000

    Conflict Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import {Weather} from "./components/weather-page/Weather";
-import {MissionPage} from './Mission-Page'
-
 import { HomePage } from './Home';
 import { MarsRover } from './rover/marsRover';
 import { MissionPage } from './components/missions-page/MissionPage';
