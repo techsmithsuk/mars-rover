6fc64c14ff480642f92bbe8a81820c413f687a1d
Jake Horton <jake.horton@softwire.com>
2019-11-28 16:09:34 +0000

    Truncated Pop-Up Text Description

--- src/components/missions-page/MissionDescription.tsx
+++ src/components/missions-page/MissionDescription.tsx
@@ -7,6 +7,10 @@ interface MissionDescriptionProps {
     mission: Mission | undefined;
 }
 
+function truncateString(string : string, number : number) {
+    return string.slice(0, number) + "...";
+  }
+
 export function MissionDescription(props: MissionDescriptionProps) {
     if (!props.mission) {
         return <div>Couldn't find that mission</div>
@@ -18,7 +22,7 @@ export function MissionDescription(props: MissionDescriptionProps) {
             <div className="info">
                 <p className="launch">Launched: {props.mission.launched}</p>
                 <p className="launch">Landed: {props.mission.landed}</p>
-                <p>{props.mission.description}</p>
+                <p>{truncateString(props.mission.description, 250)}</p>
                 <a  className="moreInfo" href={props.mission.moreInfoUrl}>More info</a>
             </div>
 
